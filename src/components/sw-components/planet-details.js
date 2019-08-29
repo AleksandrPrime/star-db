a496065cc5fcce2784c8e6961ebad0114f875100
AlexPrime <pochtarev.dev@gmail.com>
2019-08-29 23:02:16 +0300

    refactored sw-components

--- src/components/sw-components/planet-details.js
+++ src/components/sw-components/planet-details.js
@@ -4,12 +4,10 @@ import ItemDetails, { Record } from '../item-details';
 
 import { withSwapiService } from '../hoc-helpers';
 
-const PlanetDetails = ({ itemId, swapiService}) => {
-    const { getPlanet, getPlanetImage} = swapiService;
+const PlanetDetails = (props) => {
+
     return (
-        <ItemDetails itemId={itemId}
-                     getData={getPlanet}
-                     getImageUrl={getPlanetImage}>
+        <ItemDetails {...props}>
             <Record field="population" label="Population"/>
             <Record field="rotationPeriod" label="Rotation Period"/>
             <Record field="diameter" label="Diameter"/>
@@ -17,4 +15,11 @@ const PlanetDetails = ({ itemId, swapiService}) => {
     );
 };
 
-export default withSwapiService(PlanetDetails);
+const mapMethodsToProps = (swapiService) => {
+    return {
+        getData: swapiService.getPlanet,
+        getImageUrl: swapiService.getPlanetImage
+    }
+};
+
+export default withSwapiService(PlanetDetails, mapMethodsToProps);
