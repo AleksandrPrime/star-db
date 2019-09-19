8c04fbdaf3ab38e78408cec2cc471c4e050a6381
AlexPrime <pochtarev.dev@gmail.com>
2019-09-19 15:28:51 +0300

    add Router and Route in app.js rename tag "a" on "link"

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,7 +11,9 @@ import { DummySwapiService } from '../../services/dummy-swapi-service';
 import './app.css';
 
 import ErrorBoundry from "../error-boundry";
-import { PeoplePage, PlanetsPage, StarshipPage } from '../pages'
+import { PeoplePage, PlanetsPage, StarshipPage } from '../pages';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import StarshipDetails from "../sw-components/starship-details";
 
 
 export default class App extends Component {
@@ -48,17 +50,26 @@ export default class App extends Component {
         return (
             <ErrorBoundry>
                 <SwapiServiceProvider value={this.state.swapiService}>
-                    <div className="stardb-app">
-                        <Header onServiceChange={this.onServiceChange}/>
-
-                        <RandomPlanet />
-
-                        <PeoplePage />
-
-                        <PlanetsPage />
-
-                        <StarshipPage />
-                    </div>
+                    <Router>
+                        <div className="stardb-app">
+                            <Header onServiceChange={this.onServiceChange}/>
+                            <RandomPlanet/>
+                            <Route path="/"
+                                   render={() => <h2>Welcome to StarDb</h2>}
+                                   exact/>
+                            <Route path="/people"
+                                   render={() => <h2>People</h2>}
+                                   exact/>
+                            <Route path="/people" component={PeoplePage}/>
+                            <Route path="/planets" component={PlanetsPage}/>
+                            <Route path="/starship" exact component={StarshipPage}/>
+                            <Route path="/starship/:id"
+                                   render={({ match })=> {
+                                       const { id } = match.params;
+                                      return <StarshipDetails itemId={id}/>
+                                   }}/>
+                        </div>
+                    </Router>
                 </SwapiServiceProvider>
             </ErrorBoundry>
         );
