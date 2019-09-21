dcd10c66fffa606d143534fec7bbf2778677e4eb
AlexPrime <pochtarev.dev@gmail.com>
2019-09-22 00:57:53 +0300

    fix bag

---
+++ build/precache-manifest.36dfe8b5f872109842b6140abc322baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b854a5485fc8b48d3c3a732430235ca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "919fd732c39a0f502300",
+    "url": "/static/css/main.69a1db8a.chunk.css"
+  },
+  {
+    "revision": "012c773754b69065d6d5",
+    "url": "/static/js/2.c92c8403.chunk.js"
+  },
+  {
+    "revision": "919fd732c39a0f502300",
+    "url": "/static/js/main.6605cbab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
