ad39b89b00c6e373f914da3c843429c181e6d4f3
Aleksandr <golem007@gmail.com>
2021-03-29 23:30:47 +0300

    update

---
+++ build/precache-manifest.62e55a62809dc912f65106b46c4e2f46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcddbfedbb7cee4dab5f86965aa3b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe826974e9d0619fe7d",
+    "url": "/static/css/main.98dfadd9.chunk.css"
+  },
+  {
+    "revision": "a596c3cf2655aee60eea",
+    "url": "/static/js/2.f11a7e31.chunk.js"
+  },
+  {
+    "revision": "dbe826974e9d0619fe7d",
+    "url": "/static/js/main.1e3a31b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
